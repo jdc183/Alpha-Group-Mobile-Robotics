6b863574ab6b9688a07e15f86a3d22e81d12ab7d
nicolemariegraf <nmg63@case.edu>
2021-02-16 15:32:20 -0500

    Added code to wall follower tracker

--- PS2/wall_follower_package/src/wall_follower_tracker.cpp
+++ PS2/wall_follower_package/src/wall_follower_tracker.cpp
@@ -24,4 +24,23 @@ int main(int argc, char **argv) {
     	// if triggeres wall follower navigator or lidar alarm
     		// run wall follower spinnner until signal goes away
     // that should be it?
+
+
+    // from heading_test_client.cpp in order to talk to the service
+    ros::init(argc, argv, "heading_test_client");
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<double_vec_srv::DblVecSrv>("stdr_rotation_service");
+    double_vec_srv::DblVecSrv srv;
+    double desired_heading;
+    while(ros::ok()) {
+        cout<<"enter a desired heading: ";
+        cin>>desired_heading;
+        srv.request.vec_of_doubles.resize(1);
+        srv.request.vec_of_doubles[0]=desired_heading;
+        //alt:
+        //srv.request.vec_of_doubles.clear();
+        //srv.request.vec_of_doubles.push_back(desired_heading);
+        client.call(srv);  
+    }
+    return 0;
 }
