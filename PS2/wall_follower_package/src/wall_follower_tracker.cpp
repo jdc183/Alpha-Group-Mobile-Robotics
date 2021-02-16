7455bc6f2204d0a878908ed1dc754ca2cd01420f
nicolemariegraf <nmg63@case.edu>
2021-02-16 13:42:53 -0500

    Added some initial code and more comments to wall_follower_tracker

--- PS2/wall_follower_package/src/wall_follower_tracker.cpp
+++ PS2/wall_follower_package/src/wall_follower_tracker.cpp
@@ -4,3 +4,23 @@
 	//Have robot move forward with twist vectors like PS1
 	// Have if statements that are triggered by the statuses of the alarm and navigator programs
 		// Reset the variables after execution of the processes
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <std_msgs/Float32.h> //Including the Float32 class from std_msgs
+#include <std_msgs/Bool.h> // boolean message 
+#include <math.h>
+#define PI = M_PI;
+	
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "stdr_commander"); // initialize a new node
+    ros::NodeHandle n; // two lines to create a publisher object that can talk to ROS
+    ros::Publisher twist_commander = n.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1); // set name you use when you publish. declare the data type.
+
+    // while not triggering lidar alarm
+    	// while not triggering wall follower navigator
+    		// move forward via twist command stuff
+    	// if triggeres wall follower navigator or lidar alarm
+    		// run wall follower spinnner until signal goes away
+    // that should be it?
