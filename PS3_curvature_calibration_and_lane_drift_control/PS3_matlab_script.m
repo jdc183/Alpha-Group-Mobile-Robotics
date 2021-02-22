131cfcb7cf7a9f81c8adbc7a33d4a6b96d18388d
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-02-22 00:48:06 -0500

    Added some calculations, curvature seems incorrect

--- PS3_curvature_calibration_and_lane_drift_control/PS3_matlab_script.m
+++ PS3_curvature_calibration_and_lane_drift_control/PS3_matlab_script.m
@@ -1,47 +1,49 @@
 % Nicole Graf, Joseph Cressman, and Andrew Capelli
 % PS3: 
 % Due 24 February 2021
-close all; clear all; clc
-%% Reading the CSVs
-% Code from Newman
-data = load("circle15.csv");
-lat_start = data(1,1) %for convenience, compute x,y motions relative to starting location 
-lon_start = data(1,2)
-    % convert lat/lon to x,y w/rt starting position
-    % magic numbers to convert deg lat and deg lon to dy and dx
-    % NOTE: GPS calls longitude -81deg, not 81deg "West"
-    % so longitude gets more positive heading east, = x direction
-    % latitude gets more positive heading north, = y direction
-% y_vec = (data(:,1)-lat_start)*lat_to_meters;
-% x_vec = (data(:,2)-lon_start)*lon_to_meters_at_41degN;
+% close all; clear all; clc
 
+r_earth = 6.371e6;
+lat_to_meters = r_earth*pi/180;
+lon_to_meters_at_41degN = r_earth*cos(41*pi/180)*pi/180;
 %% Reading the CSVs Nicole's way
-all_lat = [];
-all_lon = [];
-all_steering_angle = [];
- for i = 1:1:15 %update to reflect names of data
-    filename = ['circle', (num2str(i)), '.csv'];
+figure;
+for i = 1:15 %update to reflect names of data
+    filename = ['trimdcircle', (num2str(i)), '.csv'];
     data = csvread(filename);
     % syntax for getting data
-    lat_start = data(:,1);
-    lon_start = data(:,2);
+    lat_start = data(1,1);
+    lon_start = data(1,2);
+    
+    % coordinates in meters
+    y = (data(:,1)-lat_start)*lat_to_meters;
+    x = (data(:,2)-lon_start)*lon_to_meters_at_41degN;
+    
+    n = size(y,1);
     steering_angle = data(:,3);
     
-    % generating plots of the data (all 15 datas)
-    % figure(i)
-    % clf(i)
-    % figure(i)
-hold on
-    % plot(time,load); Save this for later... put plots here
-    %all_lat = [all_lat, lat_start];
-    %all_lon = [all_lon, lon_start];
-    %all_steering_angle = [all_steering_angle, steering_angle];
-% save jpeg syntax save as filename
-    % picturename = ['data', num2str(i), '.jpeg'];
-    % saveas (figure(i),picturename)
- end
-%  all_lats' 
-%  pause
-%  all_lon'
-%  pause
-%  all_steering_angle'
+    % 1st and 2nd derivatives of x and y
+    dx = diff(x);
+    dy = diff(y);
+    d2x = diff(dx);
+    d2y = diff(dy);
+    
+    % Adjust size of 1st derivatives to match 2nd
+    dx = (dx(1:n-2)+dx(2:n-1))/2;
+    dy = (dy(1:n-2)+dy(2:n-1))/2;
+    
+    % Compute curvature
+    curvature = (dx.*d2y-dy.*d2x)./(dx.^2+dy.^2).^(3/2);
+    
+    %Resize steering angle to match
+    sa = steering_angle;
+    sa = (sa(1:n-2)+sa(2:n-1)+sa(3:n))/3;
+    
+    % Plot curvature against steering angle
+    plot(sa,curvature,'*')
+    hold on
+    
+    % Plot x,y path of vehicle
+%     plot(x,y);
+%     hold on
+end
