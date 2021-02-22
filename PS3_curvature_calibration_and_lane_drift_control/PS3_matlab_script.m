7b880eb8af68b696665f59fb5a6dd5299a26437c
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-02-22 11:47:36 -0500

    Remove some outliers and compute regression

--- PS3_curvature_calibration_and_lane_drift_control/PS3_matlab_script.m
+++ PS3_curvature_calibration_and_lane_drift_control/PS3_matlab_script.m
@@ -2,13 +2,14 @@
 % PS3: 
 % Due 24 February 2021
 
+allcurv = [];
+allsteer= [];
+
 %Compute the constants to convert between degrees lat/lon to meters x/y
 r_earth = 6.371e6;
 lat_to_meters = r_earth*pi/180;
 lon_to_meters_at_41degN = r_earth*cos(41*pi/180)*pi/180;
-
-% Reading the CSVs Nicole's way
-figure;
+%% Reading the CSVs Nicole's way
 for i = 1:15 %update to reflect names of data
     filename = ['trimdcircle', (num2str(i)), '.csv'];
     data = csvread(filename);
@@ -30,28 +31,37 @@ for i = 1:15 %update to reflect names of data
     
     %Resize 1st derivative
     dx = (dx(1:n-2)+dx(2:n-1))/2;
-    % Need this for non-trimmed csv
-    % dx = dx + 0.0000001; % approximation to avoid dx = 0
     dy = (dy(1:n-2)+dy(2:n-1))/2;
-    % Need this for non-trimmed csv
-    % dy = dy + 0.0000001; % approximation to avoid dy = 0
     
-    % Compute the curvatrue
+    % Compute the curvature
     curvature = (dx.*d2y-dy.*d2x)./(dx.^2+dy.^2).^(3/2);
     
     % Resize the steering angle vector
-    sa = steering_angle;
-    sa = (sa(1:n-2)+sa(2:n-1)+sa(3:n))/3;
+    steering_angle = (steering_angle(1:n-2)+steering_angle(2:n-1)+steering_angle(3:n))/3;
     
     
-    win = 50; % Width of window function
+    win=50;%Width of window function
     
     %Smooth out the noisy data
     curvature = conv(curvature,ones(win,1)/win,'same');
-    sa = conv(sa,ones(win,1)/win,'same');
+    steering_angle = conv(steering_angle,ones(win,1)/win,'same');
     
-    plot(sa, curvature, 'o')
-    hold on
+    %remove remaining outliers:
+    steering_angle(abs(curvature)>0.25) = [];
+    curvature(abs(curvature)>0.25) = [];
+    
+%     plot(steering_angle,curvature,'o')
+%     hold on
+    
+    allcurv = [allcurv; curvature];
+    allsteer= [allsteer; steering_angle];
 %     plot(x,y);
 %     hold on
 end
+figure
+plot(allsteer,allcurv,'o');
+hold on
+reg = polyfit(allsteer,allcurv,1);
+sa = min(allsteer):100:max(allsteer);
+plot(sa,polyval(reg,sa));
+
