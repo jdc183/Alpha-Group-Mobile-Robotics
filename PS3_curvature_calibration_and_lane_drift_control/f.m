0a4ba02c2a1096a26531bcbe31296b5e40fb453a
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-02-23 16:12:26 -0500

    Added controller and simulator

---
+++ PS3_curvature_calibration_and_lane_drift_control/f.m
@@ -0,0 +1,10 @@
+function dxdt = f(x,u,t)
+    
+    dxdt = zeros(3,1);
+    
+    dxdt(1) = x(4)*cos(x(3));%dx/dt
+    dxdt(2) = x(4)*sin(x(3));%dy/dt
+    dxdt(3) = u(1)*x(4);%dpsi/dt
+    dxdt(4) = u(2);%scalar acceleration dspeed/dt (=0 with our controller)
+   
+end
---
+++ PS3_curvature_calibration_and_lane_drift_control/rk4.m
@@ -0,0 +1,15 @@
+function [slope,dt_next] = rk4(f,u,x,t,dt)
+
+    s1 = f(x,u(x,t),t);
+    s2 = f(x+dt*s1/2,u(x+dt*s1/2,t+dt/2),t+dt/2);
+    s3 = f(x+dt*s2/2,u(x+dt*s2/2,t+dt/2),t+dt/2);
+    s4 = f(x+dt*s3,u(x+dt*s3,t+dt),t+dt);
+    slope = (s1+2*s2+2*s3+s4)/6;
+    
+    alpha = 0.95;
+    error = norm((x+dt*slope) - (x+dt*slope/2 + s2*dt/2));
+    
+    R=(1e-3+(1e-4)*norm(x+slope*dt)/abs(error))^(1/(4+1));
+    
+    dt_next = alpha*R*dt;
+end
