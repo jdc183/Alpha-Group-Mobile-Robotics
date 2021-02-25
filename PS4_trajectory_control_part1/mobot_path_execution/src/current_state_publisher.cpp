261f66413d5a1f002c067bc1a05f1a2de0eec814
Nicole Graf <nicolegraf@Nicoles-MacBook.local>
2021-02-25 15:08:17 -0500

    Initial current sttate publisher code

--- PS4_trajectory_control_part1/mobot_path_execution/src/current_state_publisher.cpp
+++ PS4_trajectory_control_part1/mobot_path_execution/src/current_state_publisher.cpp
@@ -5,3 +5,20 @@
 // high-speed Odom information.  For this assignment, this 
 // node should merely subscribe to the Odom topic and 
 // republish Odom on the topic “current_state”.  
+
+#include <ros/ros.h> //Must include this for all ROS cpp projects
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "current_state_publisher"); //name this node
+    ros::NodeHandle nh; 
+    //create a Subscriber object and have it subscribe to the lidar topic
+    ros::Publisher pub = nh.advertise<std_msgs::Bool>("current_state", 1);
+    odom_publisher = pub; // let's make this global, so callback can use it
+    // ros::Publisher pub2 = nh.advertise<std_msgs::Float32>("odom_dist", 1);  
+    // lidar_dist_publisher_ = pub2;
+    ros::Subscriber lidar_subscriber = nh.subscribe("robot0/laser_0", 1, laserCallback);
+    ros::spin(); //this is essentially a "while(1)" statement, except it
+    // forces refreshing wakeups upon new data arrival
+    // main program essentially hangs here, but it must stay alive to keep the callback function alive
+    return 0; // should never get here, unless roscore dies
+}
