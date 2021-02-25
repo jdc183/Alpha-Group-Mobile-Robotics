97d2697f6f1abd6d70dd11d6e34eccedddf2eab7
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-02-25 15:14:37 -0500

    "comments"

--- PS2/single_node_wall_follower/src/wall_follower.py
+++ PS2/single_node_wall_follower/src/wall_follower.py
@@ -1,16 +1,29 @@
+<<<<<<< HEAD
+#!/usr/bin/env python3
+=======
 #!/usr/bin/env python
 
 # roslaunch xml file: package, type = executable.py, name = node, elements
+>>>>>>> 261f66413d5a1f002c067bc1a05f1a2de0eec814
 import roslib; roslib.load_manifest("single_node_wall_follower")
 import rospy
 import math
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+global linVelGain
+global angVelGain
+
+# These variables determine how sensitive the throttle and steering
+# are to the mapped lidar data
+linVelGain = 10;
+angVelGain = 20;
 
 # Callback for receipt of laser data
 def on_lidar(laser):
 	global pub
+	global linVelGain
+	global angVelGain
 
 	cmd_vel = Twist()# Create a new Twist instance
 	cmd_vel.angular.z = 0.0	# Set rotational speed to zero (for now)
@@ -19,19 +32,23 @@ def on_lidar(laser):
 	# Modify linear and rotational velocities according to lidar
 	for n in range(len(laser.ranges)):
 	 	angle = laser.angle_increment*n + laser.angle_min # calculate angle of each laser beam
-	 	dist = laser.ranges[n]
+	 	dist = laser.ranges[n] # read in the lenght of each laser beam
+
+	 	# These two lines are mostly for dealing with Inf distances
 	 	if dist > 1.8:
 	 		dist = 1.8
-	 	#Compute a cartesian point cloud from the lidar data
+
+	 	# Compute a cartesian point cloud from the lidar data
 	 	x = dist*math.cos(angle)
 	 	y = dist*math.sin(angle)
 
-	 	#Velocity adjustments are calculated by mapping each point 
-	 	linadjust = -1/((((8*(x-.25))**20)+1)*((4*y)**20+1))
-	 	angadjust = -1/((((4*(x-.5))**20)+1)*((4*y)**20+1))+1/((((4*(x-.5))**20)+1))*(1/(((1*(y-1.5))**20+1)))+1/((((2*(x-.75))**20)+1))*(-1/(((16*(y-0.25))**20)+1))
+	 	# Velocity adjustments are calculated by mapping each point 
+	 	linadjust = mapXYtoLinearSpeed(x,y)
+	 	angadjust = mapXYtoAngularVelocity(x,y)
 
-	 	cmd_vel.linear.x += 10*linadjust/len(laser.ranges)
-	 	cmd_vel.angular.z += 20*angadjust/len(laser.ranges)
+	 	# Set the velocities according to the maps
+	 	cmd_vel.linear.x += linVelGain*linadjust/len(laser.ranges)
+	 	cmd_vel.angular.z += angVelGain*angadjust/len(laser.ranges)
 
 	# Don't let either velocity exceed reasonable bounds or the sim will stop working
 	if cmd_vel.linear.x > 1:
@@ -44,7 +61,7 @@ def on_lidar(laser):
 		cmd_vel.angular.z = -6
 
 
-        
+    # command the robot
 	pub.publish(cmd_vel)
 
 # Main
@@ -59,6 +76,15 @@ def wall_follow():
 	lidar_sub = rospy.Subscriber(str(robot) + "/laser_0", LaserScan, on_lidar)
 	rospy.spin()
 
+
+
+# It would be more efficient to use booleans to check whether the wall is in specific
+# regions around the robot, but polynomials are continuous and allow for smoother feedback.
+# Also, the fact that the robot's behavior is based on a weighted average of all the laser
+# measurments means that it would me much less sensitive to measurement noise and glitches.
+
+# A rational polynomial map to compute adjustments to the forward velocity based
+# on the x,y coordinates of each obstruction around it
 def mapXYtoLinearSpeed(x,y):
 	return -1/((((8*(x-.25))**20)+1)*((4*y)**20+1))
 
@@ -74,12 +100,13 @@ def mapXYtoLinearSpeed(x,y):
 #             XXXXXX---          
 #             XXXXXX---          
 #              XXXX ---          
-                           
+# Basically, the robot will always drive forward, but will slow down, stop, or even backtrack when something is
+# directly in front of it
 
-
+# A rational polynomial map to compute adjustments to the angular velocity based
+# on the x,y coordinates of each obstruction around it
 def mapXYtoAngularVelocity(x,y):
 	return -1/((((4*(x-.5))**20)+1)*((4*y)**20+1))+1/((((4*(x-.5))**20)+1))*(1/(((1*(y-1.5))**20+1)))+1/((((2*(x-.75))**20)+1))*(-1/(((16*(y-0.25))**20)+1))
-
 #Here's a visual representation using the same method as above
 #                   ++++++       
 #                   ++++++       
@@ -91,12 +118,17 @@ def mapXYtoAngularVelocity(x,y):
 #                   ++++++       
 #                   ++++++       
 #                   ++++++        
-#                   --------------
+#                   -------------
 #              XXXX ------        
 #             XXXXXX------        
 #             XXXXXX------        
 #              XXXX ------        
+# So the robot will turn ccw if the wall on its left is to far away, and right if it's to close. 
+# It will also turn right if something is to close in the front.
 
+
+
+# code that runs immediately when this file is executed:
 if __name__ == "__main__":
 	try:
 		wall_follow()
