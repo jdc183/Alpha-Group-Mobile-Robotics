501961b8bf927fa723fd0020274579d902cd0ea5
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-02-25 15:48:33 -0500

    Added odom callback

--- PS4_trajectory_control_part1/mobot_path_execution/src/current_state_publisher.cpp
+++ PS4_trajectory_control_part1/mobot_path_execution/src/current_state_publisher.cpp
@@ -10,20 +10,25 @@
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float32.h> //Including the Float32 class from std_msgs
 #include <std_msgs/Bool.h> // boolean message 
+#include <nav_msgs/Odometry.h>
 
 ros::Publisher odom_publisher;
+
+void odomCallback(const nav_msgs::Odometry odom_message){
+    odom_publisher.publish(odom_message);
+}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "current_state_publisher"); //name this node
     ros::NodeHandle nh; 
     //create a Subscriber object and have it subscribe to the lidar topic
-    ros::Publisher pub = nh.advertise<std_msgs::Bool>("current_state", 1);
+    ros::Publisher pub = nh.advertise<nav_msgs::Odometry>("current_state", 1);
     odom_publisher = pub; // let's make this global, so callback can use it
 
-    ros::Publisher pub2 = nh.advertise<std_msgs::Float32>("odomCallback", 1);  
-    odomCallback = pub2;
+    // ros::Publisher pub2 = nh.advertise<std_msgs::Float32>("odomCallback", 1);  
+    // odomCallback = pub2;
 
-    ros::Subscriber odom_subscriber = nh.subscribe("robot0/laser_0", 1, odomCallback); // edit for mobot odom
+    ros::Subscriber odom_subscriber = nh.subscribe(/*mobot/odom*/"robot0/laser_0", 1, odomCallback); // edit for mobot odom
     ros::spin(); //this is essentially a "while(1)" statement, except it
     // forces refreshing wakeups upon new data arrival
     // main program essentially hangs here, but it must stay alive to keep the callback function alive
