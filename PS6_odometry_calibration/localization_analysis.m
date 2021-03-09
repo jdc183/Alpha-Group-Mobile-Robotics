0f126b0d70fdc190617377aa2437d951fd999a4a
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-03-09 16:53:16 -0500

    update localization_analysis.m, scale_steering_to_curvature could use more tuning

--- PS6_odometry_calibration/localization_analysis.m
+++ PS6_odometry_calibration/localization_analysis.m
@@ -1,36 +1,47 @@
 %localization analysis
-clear all
-dy_dlat = 111320
-dx_dlon = 83351  %this depends on your latitude; number here is for Cleveland
+% clear all
+% dy_dlat = 111320
+% dx_dlon = 83351  %this depends on your latitude; number here is for Cleveland
 
-heading_odom_init = 0.0; %tune this
-scale_steering_to_curvature =1; %this is not true; TUNE ME
-scale_tics_per_meter = 1; %ALSO not true; TUNE ME
+r_earth = 6.368977e6;
+dy_dlat = r_earth * pi/180;
+dx_dlon = r_earth * cos(41.4308409033 * pi/180) * pi/180;
+
+
+scale_steering_to_curvature =1.0e-03*0.023522898041216;%From PS3
+scale_steering_to_curvature = scale_steering_to_curvature*1.061;
 
 %load a trip file w/ gps and can data:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load("slalom_data.csv");
+[npts,ncols] = size(data);
 format long
-lat_start = data(1,1)
-lon_start = data(1,2)
+lat_start = data(1,1);
+lon_start = data(1,2);
 lat_vec = data(:,1); %latitude measurements
 lon_vec = data(:,2); %longitude measurements
+steering_can_vec = data(:,3); %raw steering data is in column 3
 %convert lat/lon to x,y w/rt starting position
-%magic numbers to convert deg lat and deg lon to dy and dx
-%NOTE: GPS calls longitude -81deg, not 81deg "West"
-%so longitude gets more positive heading east, = x direction
-%latitude gets more positive heading north, = y direction
 y_vec_gps = (data(:,1)-lat_start)*dy_dlat;
 x_vec_gps = (data(:,2)-lon_start)*dx_dlon;
+
+heading_odom_init = atan2(y_vec_gps(70),x_vec_gps(70)); %initial heading from gps
+
+gps_arclength = sum(sqrt(diff(x_vec_gps).^2+diff(y_vec_gps).^2));
+
 enc = (data(:,4)-data(1,4));  %encoder values are in column 4
-%enc_dist = enc/scale_tics_per_meter;  %should convert encoder values to distance w/ scale factor and offset
+enc_arclength = enc(npts);
+
+scale_tics_per_meter = enc_arclength/gps_arclength;
+
+enc_dist = enc/scale_tics_per_meter;  %should convert encoder values to distance w/ scale factor and offset
 t_vec = data(:,5);
 figure(1)
 plot(t_vec,x_vec_gps,t_vec,y_vec_gps)
 title('GPS x and y vs time')
 
-[npts,ncols] = size(data);
-steering_can_vec = data(:,3); %raw steering data is in column 3
+
+
 
 figure(2)
 plot(x_vec_gps,y_vec_gps,'b')
@@ -48,9 +59,9 @@ y_odom_vec = [y_odom_start];
 heading_odom_vec = [heading_odom];
 [npts,dummy] = size(x_vec_gps)
 for i=2:npts
-  heading_odom_vec(i)=0; %NOT true
-  x_odom_vec(i) = 0; %definitely not true
-  y_odom_vec(i) = 0; %also not true
+  heading_odom_vec(i)=heading_odom_vec(i-1)+steering_can_vec(i)*scale_steering_to_curvature*(enc_dist(i)-enc_dist(i-1)); %dpsi = kappa*dr
+  x_odom_vec(i) = x_odom_vec(i-1)+cos(heading_odom_vec(i))*(enc_dist(i)-enc_dist(i-1)); %dx = dr*cos(psi)
+  y_odom_vec(i) = y_odom_vec(i-1)+sin(heading_odom_vec(i))*(enc_dist(i)-enc_dist(i-1)); %dy = dr*sin(psi)
 end
 plot(x_odom_vec,y_odom_vec,'r')
 
