4122ea1c299c03d183d4a38f1cdc7297e1f6759e
jdc183 <54681412+jdc183@users.noreply.github.com>
2021-03-09 17:11:28 -0500

    Finer tuning of heading and curvature

--- PS6_odometry_calibration/localization_analysis.m
+++ PS6_odometry_calibration/localization_analysis.m
@@ -1,5 +1,5 @@
 %localization analysis
-% clear all
+ clear all
 % dy_dlat = 111320
 % dx_dlon = 83351  %this depends on your latitude; number here is for Cleveland
 
@@ -9,7 +9,7 @@ dx_dlon = r_earth * cos(41.4308409033 * pi/180) * pi/180;
 
 
 scale_steering_to_curvature =1.0e-03*0.023522898041216;%From PS3
-scale_steering_to_curvature = scale_steering_to_curvature*1.061;
+scale_steering_to_curvature = scale_steering_to_curvature*1.0632;
 
 %load a trip file w/ gps and can data:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,7 +25,7 @@ steering_can_vec = data(:,3); %raw steering data is in column 3
 y_vec_gps = (data(:,1)-lat_start)*dy_dlat;
 x_vec_gps = (data(:,2)-lon_start)*dx_dlon;
 
-heading_odom_init = atan2(y_vec_gps(70),x_vec_gps(70)); %initial heading from gps
+heading_odom_init = atan2(y_vec_gps(700),x_vec_gps(700))*.97; %initial heading from gps
 
 gps_arclength = sum(sqrt(diff(x_vec_gps).^2+diff(y_vec_gps).^2));
 
@@ -58,11 +58,10 @@ x_odom_vec = [x_odom_start];
 y_odom_vec = [y_odom_start];
 heading_odom_vec = [heading_odom];
 [npts,dummy] = size(x_vec_gps)
+
 for i=2:npts
   heading_odom_vec(i)=heading_odom_vec(i-1)+steering_can_vec(i)*scale_steering_to_curvature*(enc_dist(i)-enc_dist(i-1)); %dpsi = kappa*dr
   x_odom_vec(i) = x_odom_vec(i-1)+cos(heading_odom_vec(i))*(enc_dist(i)-enc_dist(i-1)); %dx = dr*cos(psi)
   y_odom_vec(i) = y_odom_vec(i-1)+sin(heading_odom_vec(i))*(enc_dist(i)-enc_dist(i-1)); %dy = dr*sin(psi)
 end
 plot(x_odom_vec,y_odom_vec,'r')
-
-
