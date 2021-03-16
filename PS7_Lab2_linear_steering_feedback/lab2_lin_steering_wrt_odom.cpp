07ffdfa966e3a1a953d252af5e5bebf92e0c76c1
Nicole Graf <nicolegraf@Nicoles-MacBook.local>
2021-03-16 13:43:05 -0400

    added comment to where i made changes to odom file

--- PS7_Lab2_linear_steering_feedback/lab2_lin_steering_wrt_odom.cpp
+++ PS7_Lab2_linear_steering_feedback/lab2_lin_steering_wrt_odom.cpp
@@ -224,9 +224,10 @@ void SteeringController::lin_steering_algorithm() {
      // do something clever with this information     
     controller_speed = des_state_vel_ + K_TRIP_DIST*trip_dist_err; //speed up/slow down to null out 
     //controller_omega = des_state_omega_; //ditto
+    // LAB 7 CHANGES
     //controller_omega = des_state_omega_ + K_PHI*heading_err + K_DISP*lateral_err;
     controller_omega = des_state_omega_ + K_PHI*heading_err + K_PHI_D * (des_state_omega_- odom_omega_)
-
+    // END LAB 7 CHANGES
     controller_omega = MAX_OMEGA*sat(controller_omega/MAX_OMEGA); // saturate omega command at specified limits
     
     // send out our very clever speed/spin commands:
