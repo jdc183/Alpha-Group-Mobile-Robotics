b86e1e8816b06890403e3120b6b5e9ddb291c8fe
acapelli345@gmail.com <acapelli345@gmail.com>
2021-04-06 16:47:07 -0400

    add skeletons for controller, nav service, backup service, and srv folder

---
+++ alpha_final/src/nav_controller.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <mobot_pub_des_state/path.h>
+#include <ps8_mobot_pub_des_state/path.h>
+#include <mobot_pub_des_state/path.h>
+#include <iostream>
+#include <string>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+
+//globals
+
+
+//helper functions
+
+
+//callbacks
+
+
+//main function
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "append_path_client");
+    ros::NodeHandle n;
+    
+
+    return 0;
+}
