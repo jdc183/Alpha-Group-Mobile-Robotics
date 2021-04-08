07e2b9dac9b41e8ca215dcf6cb19838a59b1f71b
acapelli345@gmail.com <acapelli345@gmail.com>
2021-04-08 01:39:37 -0400

    major edits to nav_controller, backup_service to make them nearly complete and 100% buildable, pending debug

--- alpha_final/src/backup_service.cpp
+++ alpha_final/src/backup_service.cpp
@@ -1,220 +1,98 @@
-// Nicole Graf, Joseph Cressman, Andrew Capelli
-
-// This code is a service to have the Jinx robot reverse 1 meter.
-
-// i started by setting the accel_max and speed_max to be negative. I also edited down the entire pub_des_state file tto what I believe that we need. 
-// To make sure that this code will run, I checked the traj_builder and noticed an issue with the triangular travel traj builder. I copied the portion over that we need and corrected these issues.
-// I ~believe~ i have the correct components hardcoded to 1 meter (path_queue), but I am not sure becaause this code technically does not have a path queue
-	// I made a set variable (bkwd_dist_desired) to accomplish this. I thought iit would be easier to change this amount at the top of our code if we want to change this value at a later point.
-
 //include statements
 #include <ros/ros.h>
-#include <mobot_pub_des_state/path.h>
 #include <iostream>
 #include <string>
+#include <std_msgs/Float32.h> //Including the Float32 class from std_msgs
+#include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h> // boolean message 
 #include <nav_msgs/Path.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
-//#include "pub_des_state.h" // i don't know if we want to use this
-#include "ps8_pub_des_state.h" // i don't know if we want to use this
+#include <sensor_msgs/LaserScan.h>
+#include <std_srvs/Trigger.h>
+#include <math.h>
+
+#include <traj_builder/traj_builder.h> 
+#include <mobot_pub_des_state/path.h>
+#include <odom_tf/odom_tf.h>
+#include <xform_utils/xform_utils.h>
+
+//#include <alpha_final/BackupService.h>
 
 //globals
-DesStatePublisher::DesStatePublisher(ros::NodeHandle& nh) : nh_(nh) {
-    //as_(nh, "pub_des_state_server", boost::bind(&DesStatePublisher::executeCB, this, _1),false) {
-    //as_.start(); //start the server running
-    //configure the trajectory builder: 
-    //dt_ = dt; //send desired-state messages at fixed rate, e.g. 0.02 sec = 50Hz
-    trajBuilder_.set_dt(dt);
-    //dynamic parameters: should be tuned for target system
-    accel_max_ = -accel_max; //absolute accel_max?
-    trajBuilder_.set_accel_max(accel_max_);
-    alpha_max_ = alpha_max;
-    trajBuilder_.set_alpha_max(alpha_max_);
-    speed_max_ = -speed_max;
-    trajBuilder_.set_speed_max(speed_max_);
-    omega_max_ = 0;
-    trajBuilder_.set_omega_max(omega_max_);
-    path_move_tol_ = path_move_tol;
-    trajBuilder_.set_path_move_tol_(path_move_tol_);
-    initializePublishers();
-    initializeServices();
-    // set desired distance to be 1 meter
-    bkwd_dist_desired = 1.0; // 1m desired
-    //define a halt state; zero speed and spin, and fill with viable coords
-    halt_twist_.linear.x = 0.0;
-    halt_twist_.linear.y = 0.0;
-    halt_twist_.linear.z = 0.0;
-    halt_twist_.angular.x = 0.0;
-    halt_twist_.angular.y = 0.0;
-    halt_twist_.angular.z = 0.0;
-    current_pose_ = trajBuilder_.xyPsi2PoseStamped(0,0,0);
-    start_pose_ = current_pose_;
-    end_pose_ = current_pose_;
-    current_des_state_.pose.pose = current_pose_.pose;
-    seg_start_state_ = current_des_state_;
-    seg_end_state_ = current_des_state_;
-}
+ros::ServiceClient pubdesClient;
+mobot_pub_des_state::path path_srv;
+geometry_msgs::PoseStamped est_st_pose_base_wrt_map;
 
-// from traj builder so we do not have to worry about issues with negative acceleration and speed from up top!
-void build_triangular_travel_traj(geometry_msgs::PoseStamped start_pose,
-        geometry_msgs::PoseStamped end_pose,
-        std::vector<nav_msgs::Odometry> &vec_of_states) {
-    double x_start = start_pose.pose.position.x;
-    double y_start = start_pose.pose.position.y;
-    double x_end = end_pose.pose.position.x;
-    double y_end = end_pose.pose.position.y;
-    double dx = x_end - x_start;
-    double dy = y_end - y_start;
-    double psi_des = atan2(dy, dx);
-    nav_msgs::Odometry des_state;
-    des_state.header = start_pose.header; //really, want to copy the frame_id
-    des_state.pose.pose = start_pose.pose; //start from here
-    des_state.twist.twist = halt_twist_; // insist on starting from rest
-    double trip_len = sqrt(dx * dx + dy * dy);
-    double t_ramp = sqrt(trip_len / (-accel_max_));
-    int npts_ramp = round(t_ramp / dt_);
-    double v_peak = accel_max_*t_ramp; // could consider special cases for reverse motion
-    double d_vel = alpha_max_*dt_; // incremental velocity changes for ramp-up
 
-    double x_des = x_start; //start from here
-    double y_des = y_start;
-    double speed_des = 0.0;
-    des_state.twist.twist.angular.z = 0.0; //omega_des; will not change
-    des_state.pose.pose.orientation = convertPlanarPsi2Quaternion(psi_des); //constant
-    // orientation of des_state will not change; only position and twist
-    double t = 0.0;
-    //ramp up;
-    for (int i = 0; i < npts_ramp; i++) {
-        t += dt_;
-        speed_des = accel_max_*t;
-        des_state.twist.twist.linear.x = speed_des; //update speed
-        //update positions
-        x_des = x_start + 0.5 * accel_max_ * t * t * cos(psi_des);
-        y_des = y_start + 0.5 * accel_max_ * t * t * sin(psi_des);
-        des_state.pose.pose.position.x = x_des;
-        des_state.pose.pose.position.y = y_des;
-        vec_of_states.push_back(des_state);
-    }
-    //ramp down:
-    for (int i = 0; i < npts_ramp; i++) {
-        speed_des -= accel_max_*dt_; //Euler one-step integration
-        des_state.twist.twist.linear.x = speed_des;
-        x_des += speed_des * dt_ * cos(psi_des); //Euler one-step integration
-        y_des += speed_des * dt_ * sin(psi_des); //Euler one-step integration        
-        des_state.pose.pose.position.x = x_des;
-        des_state.pose.pose.position.y = y_des;
-        vec_of_states.push_back(des_state);
-    }
-    //make sure the last state is precisely where requested, and at rest:
-    des_state.pose.pose = end_pose.pose;
-    //but final orientation will follow from point-and-go direction
-    des_state.pose.pose.orientation = convertPlanarPsi2Quaternion(psi_des);
-    des_state.twist.twist = halt_twist_; // insist on starting from rest
-    vec_of_states.push_back(des_state);
-}
-// end traj builder
+//OdomTf odomTf;
+XformUtils xform_utils;
+
+
+double g_odom_tf_x;
+double g_odom_tf_y;
+double g_odom_tf_phi;
 
 //helper functions
-//might not need 112-118
-void DesStatePublisher::initializeServices() {
-    ROS_INFO("Initializing Services");
-    flush_path_queue_ = nh_.advertiseService("flush_path_queue_service",
-            &DesStatePublisher::flushPathQueueCB, this);
-    append_path_ = nh_.advertiseService("append_path_queue_service",
-            &DesStatePublisher::appendPathQueueCB, this);
-}
 
-//member helper function to set up publishers;
-
-void DesStatePublisher::initializePublishers() {
-    ROS_INFO("Initializing Publishers");
-    desired_state_publisher_ = nh_.advertise<nav_msgs::Odometry>("/desState", 1, true);
-    des_psi_publisher_ = nh_.advertise<std_msgs::Float64>("/desPsi", 1);
-}
-
-//might not need 129-136
-bool DesStatePublisher::flushPathQueueCB(std_srvs::TriggerRequest& request, std_srvs::TriggerResponse& response) {
-    ROS_WARN("flushing path queue");
-    while (!path_queue_.empty())
-    {
-        path_queue_.pop();
-    }
-    return true;
-}
-
-bool DesStatePublisher::appendPathQueueCB(mobot_pub_des_state::pathRequest& request, mobot_pub_des_state::pathResponse& response) {
-
-    int npts = request.path.poses.size();
-    ROS_INFO("appending path queue with %d points", npts);
-    for (int i = 0; i < npts; i++) {
-        path_queue_.push(request.path.poses[i]);
-    }
-    return true;
-}
-
-void DesStatePublisher::set_init_pose(double x, double y, double psi) {
-    current_pose_ = trajBuilder_.xyPsi2PoseStamped(x, y, psi);
-}
 
 //callbacks
-// from lab 4 service
-void currentStateCallback(const nav_msgs::Odometry current){
-    g_start_pose.pose = current.pose.pose;
-    g_start_pose.header = current.header;
-}
-// end inputs from lab 4
+bool backupCB(std_srvs::TriggerRequest& request, std_srvs::TriggerResponse& response){
+    response.success=false;
+    ROS_WARN("Trigger received, moving backwards");
+    ros::NodeHandle n2;
+    OdomTf odomTf(&n2);
 
-void DesStatePublisher::pub_next_state() {
-    //state machine; results in publishing a new desired state
-	//generate triangular trajectory and start publishing it with corresponding positions that go with it and clock that out
-        //case PURSUING_SUBGOAL: //if have remaining pts in computed traj, send them
-            //extract the i'th point of our plan:
-            current_des_state_ = des_state_vec_[traj_pt_i_];
-            current_pose_.pose = current_des_state_.pose.pose;
-            current_des_state_.header.stamp = ros::Time::now();
-            desired_state_publisher_.publish(current_des_state_);
-            //next three lines just for convenience--convert to heading and publish
-            // for rqt_plot visualization            
-            des_psi_ = trajBuilder_.convertPlanarQuat2Psi(current_pose_.pose.orientation);
-            float_msg_.data = des_psi_;
-            des_psi_publisher_.publish(float_msg_); 
-            traj_pt_i_++; // increment counter to prep for next point of plan
-            //check if we have clocked out all of our planned states:
-            if (traj_pt_i_ >= npts_traj_) {
-                seg_end_state_ = des_state_vec_.back(); // last state of traj
-                //path_queue_.pop(); 	// pop off destination vertex should be hardcoded feed it one meter
-                bkwd_dist_desired = path_queue_; // i think this may be correct?
-                end_pose_
-                ROS_INFO("reversed 1m: x = %f, y= %f",current_pose_.pose.position.x,
-                        current_pose_.pose.position.y);
-            }
-            break;
+    est_st_pose_base_wrt_map = xform_utils.get_pose_from_stamped_tf(odomTf.stfEstBaseWrtMap_);
+    g_odom_tf_x = est_st_pose_base_wrt_map.pose.position.x;
+    g_odom_tf_y = est_st_pose_base_wrt_map.pose.position.y;
+    g_odom_tf_phi = xform_utils.convertPlanarQuat2Phi(est_st_pose_base_wrt_map.pose.orientation);
 
-        default: //this should not happen
-            ROS_WARN("motion mode not recognized!");
-            desired_state_publisher_.publish(current_des_state_);
-            break;
+    geometry_msgs::PoseStamped goal = est_st_pose_base_wrt_map;
+
+    if(g_odom_tf_phi>(M_PI/4) && g_odom_tf_phi<(3*M_PI/4)){
+        ROS_WARN("Instructing to back up in the +y direction...");
+        goal.pose.position.y = goal.pose.position.y + 0.5;
     }
+    else if(g_odom_tf_phi<(-M_PI/4) && g_odom_tf_phi>(-3*M_PI/4)){
+        ROS_WARN("Instructing to back up in the -y direction...");
+        goal.pose.position.y = goal.pose.position.y - 0.5;
+    }
+    else if(fabs(g_odom_tf_phi)<(M_PI/4)){
+        ROS_WARN("Instructing to back up in the -x direction...");
+        goal.pose.position.x = goal.pose.position.x - 0.5;
+    }
+    else if(fabs(g_odom_tf_phi)>(3*M_PI/4)){
+        ROS_WARN("Instructing to back up in the +x direction...");
+        goal.pose.position.x = goal.pose.position.x + 0.5;
+    }
+
+    path_srv.request.path.poses.push_back(goal);
+    ROS_WARN("sending movement request");
+    while(!pubdesClient.call(path_srv)){response.success=false;}
+
+    response.success=true;
+    return response.success;
+}
 
 //main function
 int main(int argc, char **argv) {
     ros::init(argc, argv, "backup_service");
-    ros::NodeHandle nh; //instantiate a desired-state publisher object
+    ROS_WARN("initializing  ROS and node handle");
 
-    DesStatePublisher desStatePublisher(nh);
-    //dt is set in header file pub_des_state.h    
-    ros::Rate looprate(1 / dt); //timer for fixed publication rate
-    desStatePublisher.set_init_pose(0,0,0); //x=0, y=0, psi=0
-    //put some points in the path queue--hard coded here
-    desStatePublisher.append_path_queue(5.0,0.0,0.0);
-    desStatePublisher.append_path_queue(0.0,0.0,0.0);
+    ros::NodeHandle n;
+    ROS_WARN("initializing OdomTF");
+    OdomTf odomTf(&n);
+    ROS_WARN("initializing XformUtils");
+    XformUtils xform_utils;
     
-    // main loop; publish a desired state every iteration
-    while (ros::ok()) {
-        desStatePublisher.pub_next_state();
-        ros::spinOnce();
-        looprate.sleep(); //sleep for defined sample period, then do loop again
-    }
+    ROS_WARN("establishing servers");
+    ros::ServiceClient pubdesClient = n.serviceClient<mobot_pub_des_state::path>("append_path_queue_service");
+    ros::ServiceServer service = n.advertiseService("backup",backupCB);
+
+    ros::spin();
 
     return 0;
 }
--- alpha_final/src/nav_controller.cpp
+++ alpha_final/src/nav_controller.cpp
@@ -14,102 +14,280 @@
 #include <mobot_pub_des_state/path.h>
 #include <std_msgs/Float64.h>
 
+#include <odom_tf/odom_tf.h>
+#include <xform_utils/xform_utils.h>
+
 #include <alpha_final/NavService.h>
+#include <alpha_final/BackupService.h>
 
 
 //globals
-ros::NodeHandle n;
+
+XformUtils xform_utils;
 
 ros::ServiceClient navClient;
-ros::ServiceClient backClient;
+ros::ServiceClient backupClient;
 ros::ServiceClient scanClient;
 ros::ServiceClient grabClient;
 ros::ServiceClient dropClient;
+ros::ServiceClient pubdesClient;
 
+std_srvs::Trigger trigger;
+
+double distRobotFrontToCenter = 0.2;
+double tolerance = 0.1;
+double angle_tolerance = 0.05;
+
+double goal1_x = 3.903;
+double goal1_y = 0.412;
+double goal1_phi = -0.012;
+
+double goal2_x = 0.542;
+double goal2_y = 2.572;
+double goal2_phi = 1.539;
 //callbacks
 
-bool navP2PCallback(){
 
-	return false;
+
+//helper functions
+bool pointToleranceCheck(double current, double end){
+    if((current<(end+tolerance)) && (current>(end-tolerance))){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
+bool angleToleranceCheck(double current, double end){
+    if((current<(end+angle_tolerance)) && (current>(end-angle_tolerance))){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool poseToleranceCheck(geometry_msgs::Pose current, geometry_msgs::Pose end){
+    int score=0;
+    if(pointToleranceCheck(current.position.x, end.position.x)){
+        score++;
+    }
+
+    if(pointToleranceCheck(current.position.y, end.position.y)){
+        score++;
+    }
+
+    if(pointToleranceCheck(current.position.z, end.position.z)){
+        //score++;
+    }
+
+    if(angleToleranceCheck(current.orientation.x, end.orientation.x)){
+        //score++;
+    }
+
+    if(angleToleranceCheck(current.orientation.y, end.orientation.y)){
+        //score++;
+    }
+
+    if(angleToleranceCheck(current.orientation.z, end.orientation.z)){
+        score++;
+    }
+
+    if(angleToleranceCheck(current.orientation.w, end.orientation.w)){
+        score++;
+    }
+
+    if(score==4){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi) {
+    geometry_msgs::Quaternion quaternion;
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = sin(phi / 2.0);
+    quaternion.w = cos(phi / 2.0);
+    return quaternion;
+}
+
 /*
-bool backupCallback(){
-	
-}
+void blockToArrival(double x, double y, double phi){
+  geometry_msgs::Pose goal;
+  goal.position.x=x;
+  goal.position.y=y;
+  goal.orientation = convertPlanarPhi2Quaternion(phi);
 
-bool scanCallback(){
-	
-}
+  geometry_msgs::Pose current = xform_utils.get_pose_from_stamped_tf(odomTf.stfEstBaseWrtMap_);
 
-bool grabCallback(){
-	
-}
-
-bool dropCallback(){
-	
+  bool arrived = false;
+  while(!arrived){
+    current = xform_utils.get_pose_from_stamped_tf(odomTf.stfEstBaseWrtMap_);
+    poseToleranceCheck(current, goal);
+    ros::Duration(0.25).sleep();
+  }
 }
 */
 
-//helper functions
+void initializeServices(ros::NodeHandle n){
+  ROS_WARN("initializing necessary services");
+  ros::ServiceClient pubdesClient = n.serviceClient<mobot_pub_des_state::path>("append_path_queue_service");
 
-void initializeServices(){
-	navClient = n.serviceClient<alpha_final::NavService>("nav_p2p_service");
+  /*
+	navClient = n.serviceClient<alpha_final_test::NavServiceTest>("nav_p2p_service_test");
 	while (!navClient.exists()) {
       ROS_INFO("waiting for nav service...");
       ros::Duration(1.0).sleep();
     }
-    ROS_INFO("connected nav client to service");
+    ROS_INFO("connected nav client to nav service");
+  */
 
-    /*
+	backupClient = n.serviceClient<std_srvs::Trigger>("backup");
+  while (!backupClient.exists()) {
+    ROS_WARN("waiting for backup service...");
+    ros::Duration(1.0).sleep();
+  }
+  ROS_INFO("connected backup client to backup service");
 
-	//backupClient = n.serviceClient("backup_service");
-    while (!backupClient.exists()) {
-      ROS_INFO("waiting for backup service...");
-      ros::Duration(1.0).sleep();
-    }
-    ROS_INFO("connected backup client to service");
-
+  /*
 	//RESERVED FOR FUTURE USE
 
 	scanClient = n.advertiseService("<scan service name>",scanCallback);
-    while (!scanClient.exists()) {
-      ROS_INFO("waiting for scan service...");
-      ros::Duration(1.0).sleep();
-    }
-    ROS_INFO("connected scan client to service");
+  while (!scanClient.exists()) {
+    ROS_INFO("waiting for scan service...");
+    ros::Duration(1.0).sleep();
+  }
+  ROS_INFO("connected scan client to scan service");
 
 	grabClient = n.advertiseService("<grab service name>",grabCallback);
-    while (!grabClient.exists()) {
-      ROS_INFO("waiting for grab service...");
-      ros::Duration(1.0).sleep();
-    }
-    ROS_INFO("connected grab client to service");
+  while (!grabClient.exists()) {
+    ROS_INFO("waiting for grab service...");
+    ros::Duration(1.0).sleep();
+  }
+  ROS_INFO("connected grab client to grab service");
 
 	dropClient = n.advertiseService("<drop service name>",dropCallback);
-    while (!dropClient.exists()) {
-      ROS_INFO("waiting for drop service...");
-      ros::Duration(1.0).sleep();
-    }
-    ROS_INFO("connected drop client to service");
+  while (!dropClient.exists()) {
+    ROS_INFO("waiting for drop service...");
+    ros::Duration(1.0).sleep();
+  }
+  ROS_INFO("connected drop client to drop service");
 
 	*/
+  ROS_WARN("service initialization complete");
+}
+
+//function to add 2 points to path queue for robot
+//allows robot to align with goal point, then approach 
+//xFirst boolean toggles whether robot travels in x or y direction first
+mobot_pub_des_state::path addPointXYPtoPath(mobot_pub_des_state::path path_srv, double x, double y, double phi, bool xFirst){
+  ROS_WARN("entered point-to-path function");
+  geometry_msgs::PoseStamped pose_stamped;
+  pose_stamped.header.frame_id = "world";
+
+  geometry_msgs::Quaternion quat;
+  geometry_msgs::Pose pose;
+  
+  if(!xFirst){
+    ROS_WARN("queueing points to y=%f, then x=%f",y,x);
+    pose.position.x = 0.0; // Center of robot aligned with center of dock
+    pose.position.y = y;
+    pose.position.z = 0.0; // let's hope so!
+    quat = convertPlanarPhi2Quaternion(phi);
+    pose.orientation = quat;
+    pose_stamped.pose = pose;
+    path_srv.request.path.poses.push_back(pose_stamped);
+
+    pose.position.x = x - distRobotFrontToCenter; // front of robot on center of dock
+    pose_stamped.pose = pose;
+    path_srv.request.path.poses.push_back(pose_stamped);
+  }
+  else{
+    ROS_WARN("queueing points to x=%f, then y=%f",x,y);
+    pose.position.x = x; // Center of robot aligned with center of dock
+    pose.position.y = 0.0;
+    pose.position.z = 0.0; // let's hope so!
+    quat = convertPlanarPhi2Quaternion(phi);
+    pose.orientation = quat;
+    pose_stamped.pose = pose;
+    path_srv.request.path.poses.push_back(pose_stamped);
+
+    pose.position.y = y - distRobotFrontToCenter; // front of robot on center of dock
+    pose_stamped.pose = pose;
+    path_srv.request.path.poses.push_back(pose_stamped);
+  }
+  
+  ROS_WARN("Queue created, returning path");
+  return path_srv;
 }
 
 //main function
 int main(int argc, char **argv) {
 	//ros init
-    ros::init(argc, argv, "nav_controller");
-    ros::NodeHandle n;
-    
-    //service initialization
-    initializeServices();
+  ROS_WARN("initializing  ROS and node handle");
+  ros::init(argc, argv, "nav_controller");
+  ros::NodeHandle n;
+  ROS_WARN("initializing OdomTF");
+  OdomTf odomTf(&n);
+  ROS_WARN("initializing XformUtils");
+  XformUtils xform_utils;
+  
+  //service initialization
+  initializeServices(n);
 
-    geometry_msgs::Quaternion quat;
-    
+  //defining goal paths
+  ROS_WARN("creating goal paths");
+  mobot_pub_des_state::path path_srv_goal1;
+  path_srv_goal1 = addPointXYPtoPath(path_srv_goal1, goal1_x, goal1_y, goal1_phi, false);
 
+  mobot_pub_des_state::path path_srv_goal2;
+  path_srv_goal2 = addPointXYPtoPath(path_srv_goal2, goal2_x, goal2_y, goal2_phi, true);
 
-    //mobot_pub_des_state::path path_srv;
+  mobot_pub_des_state::path path_srv_end;
+  path_srv_end = addPointXYPtoPath(path_srv_end, 0.0, 0.0, 0, false);
 
+  ROS_WARN("issuing service call to reach station 1"); 
+  pubdesClient.call(path_srv_goal1);
 
-    return 0;
+  geometry_msgs::Pose goal1;
+  goal1.position.x=goal1_x;
+  goal1.position.y=goal1_y;
+  goal1.orientation = convertPlanarPhi2Quaternion(goal1_phi);
+
+  geometry_msgs::PoseStamped current = xform_utils.get_pose_from_stamped_tf(odomTf.stfEstBaseWrtMap_);
+
+  bool arrived = false;
+  while(!arrived){
+    current = xform_utils.get_pose_from_stamped_tf(odomTf.stfEstBaseWrtMap_);
+    ROS_WARN("current position: %f, %f",current.pose.position.x,current.pose.position.y);
+    ROS_WARN("goal position: %f, %f",goal1_x,goal1_y);
+    arrived = poseToleranceCheck(current.pose, goal1);
+    ros::Duration(0.25).sleep();
+  }
+
+  /*
+  insert station 1 perception and manipulation here
+  */
+
+  ROS_WARN("issuing trigger to backup service");
+  backupClient.call(trigger);
+
+  ROS_WARN("issuing service call to reach station 2"); 
+  pubdesClient.call(path_srv_goal2);
+
+  /*
+  insert station 2 perception and manipulation here
+  */
+  
+  ROS_WARN("issuing trigger to backup service");
+  backupClient.call(trigger);
+
+  ROS_WARN("issuing service call to reach end position");
+  pubdesClient.call(path_srv_end);
+
+  return 0;
 }
