2f468158f803228f1502db9e98659dd3d36e13d4
acapelli345@gmail.com <acapelli345@gmail.com>
2021-04-28 19:18:11 -0400

    adds helper functions to centroid service

--- alpha_final/src/find_centroid_service.cpp
+++ alpha_final/src/find_centroid_service.cpp
@@ -1,26 +1,28 @@
 //include statements
 #include <ros/ros.h>
-#include <mobot_pub_des_state/path.h>
+#include <stdlib.h>
+#include <math.h>
+#include <queue>
 #include <iostream>
 #include <string>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <queue>
-#include <traj_builder/traj_builder.h> //has almost all the headers we need
 #include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
 #include <std_srvs/Trigger.h>
-#include <mobot_pub_des_state/path.h>
 #include <std_msgs/Float64.h>
-#include <math.h>
 
 #include <odom_tf/odom_tf.h>
 #include <xform_utils/xform_utils.h>
+#include <pcl_utils/pcl_utils.h>
+#include <traj_builder/traj_builder.h> //has almost all the headers we need
+#include <mobot_pub_des_state/path.h>
 
 #include <alpha_final/NavService.h>
 #include <alpha_final/BackupService.h>
+#include <alpha_final/FindCentroidService.h>
 
 //pcl includes:
 #include <sensor_msgs/PointCloud2.h> 
@@ -30,13 +32,14 @@
 #include <pcl-1.10/pcl/point_cloud.h>
 #include <pcl-1.10/pcl/PCLHeader.h>
 
-#include <pcl_utils/pcl_utils.h>
+
 
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 //#include <pcl/PCLPointCloud2.h> //PCL is migrating to PointCloud2 
 
 #include <pcl/common/common_headers.h>
+#include <pcl/common/centroid.h>
 #include <pcl/point_cloud.h>
 #include <pcl/PCLHeader.h>
 
@@ -44,17 +47,193 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 
-#include <alpha_final/FindCentroidService.h>
 
 //Global variables
-
+int g_ans;
+using namespace std;
+PclUtils *g_pcl_utils_ptr;
 
 //Helper functions
+void find_indices_of_plane_from_patch(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud_ptr, vector<int> &indices) {
+
+    pcl::PassThrough<pcl::PointXYZRGB> pass; //create a pass-through object
+    pass.setInputCloud(input_cloud_ptr); //set the cloud we want to operate on--pass via a pointer
+    pass.setFilterFieldName("z"); // we will "filter" based on points that lie within some range of z-value
+    // changed from 0.1 to 0.2; will use this as a filter until only the block is left and other table parts are filtered out.
+    pass.setFilterLimits(0.025, 0.05); //retain points with z values between these limits
+    pass.filter(indices); //  this will return the indices of the points in given cloud that pass our test
+    cout << "number of points passing the filter = " << indices.size() << endl;
+    //This fnc populates the reference arg "indices", so the calling fnc gets the list of interesting points
+}
+
+Eigen::Affine3f get_table_frame_wrt_camera() {
+    bool tferr = true;
+    int ntries = 0;
+    XformUtils xformUtils;
+    tf::TransformListener tfListener;
+    tf::StampedTransform table_frame_wrt_cam_stf;
+
+    Eigen::Affine3f affine_table_wrt_camera;
+    while (tferr) {
+        tferr = false;
+        try {
+
+            tfListener.lookupTransform("camera_depth_optical_frame", "table_frame", ros::Time(0), table_frame_wrt_cam_stf);
+        } catch (tf::TransformException &exception) {
+            ROS_WARN("%s; retrying...", exception.what());
+            tferr = true;
+            ros::Duration(0.5).sleep(); // sleep for half a second
+            ros::spinOnce();
+            ntries++;
+            if (ntries > 5) {
+                 ROS_WARN("did you launch robot's table_frame_wrt_cam.launch?");
+                 ros::Duration(1.0).sleep();
+            }
+        }
+    }
+    ROS_INFO("tf is good for table w/rt camera");
+    xformUtils.printStampedTf(table_frame_wrt_cam_stf);
+    
+
+    tf::Transform table_frame_wrt_cam_tf = xformUtils.get_tf_from_stamped_tf(table_frame_wrt_cam_stf);
+    
+    affine_table_wrt_camera = xformUtils.transformTFToAffine3f(table_frame_wrt_cam_tf);
+
+    //ROS_INFO("affine: ");
+    //xformUtils.printAffine(affine_table_wrt_camera);
+    return affine_table_wrt_camera;
+}
+
+Eigen::Affine3f get_table_frame_wrt_robot() {
+    bool tferr = true;
+    int ntries = 0;
+    XformUtils xformUtils;
+    tf::TransformListener tfListener;
+    tf::StampedTransform table_frame_wrt_bot_stf;
+
+    Eigen::Affine3f affine_table_wrt_robot;
+    while (tferr) {
+        tferr = false;
+        try {
+
+            tfListener.lookupTransform("torso", "table_frame", ros::Time(0), table_frame_wrt_bot_stf);
+        } catch (tf::TransformException &exception) {
+            ROS_WARN("%s; retrying...", exception.what());
+            tferr = true;
+            ros::Duration(0.5).sleep(); // sleep for half a second
+            ros::spinOnce();
+            ntries++;
+            if (ntries > 5) {
+                 ROS_WARN("did you launch robot's table_frame_wrt_cam.launch?");
+                 ros::Duration(1.0).sleep();
+            }
+        }
+    }
+    ROS_INFO("tf is good for table w/rt robot");
+    xformUtils.printStampedTf(table_frame_wrt_bot_stf);
+    
+
+    tf::Transform table_frame_wrt_bot_tf = xformUtils.get_tf_from_stamped_tf(table_frame_wrt_bot_stf);
+    
+    affine_table_wrt_robot = xformUtils.transformTFToAffine3f(table_frame_wrt_bot_tf);
+
+    //ROS_INFO("affine: ");
+    //xformUtils.printAffine(affine_table_wrt_camera);
+    return affine_table_wrt_robot;
+}
+
+tf::Transform cam_to_robot(){
+    bool tferr = true;
+    int ntries = 0;
+    XformUtils xformUtils;
+    tf::TransformListener tfListener;
+    tf::StampedTransform cam_to_bot;
+
+    Eigen::Affine3f affine_cam_to_bot;
+    while (tferr) {
+        tferr = false;
+        try {
+
+            tfListener.lookupTransform("head", "camera_depth_optical_frame", ros::Time(0), cam_to_bot);
+        } catch (tf::TransformException &exception) {
+            ROS_WARN("%s; retrying...", exception.what());
+            tferr = true;
+            ros::Duration(0.5).sleep(); // sleep for half a second
+            ros::spinOnce();
+            ntries++;
+            if (ntries > 5) {
+                 ROS_WARN("did you launch robot's table_frame_wrt_cam.launch?");
+                 ros::Duration(1.0).sleep();
+            }
+        }
+    }
+    ROS_INFO("tf is good for camera w/rt robot");
+    xformUtils.printStampedTf(cam_to_bot);
+    
+
+    tf::Transform cam_to_bot2 = xformUtils.get_tf_from_stamped_tf(cam_to_bot);
+    
+    affine_cam_to_bot = xformUtils.transformTFToAffine3f(cam_to_bot2);
+
+    //ROS_INFO("affine: ");
+    //xformUtils.printAffine(affine_table_wrt_camera);
+    return cam_to_bot2;
+}
+
+double plane_z_interp(float x, float y, float plane_dist, Eigen::Vector3f normal) {
+    double z;
+    z = (plane_dist - x * normal(0) - y * normal(1)) / normal(2);
+    return z;
+}
+
+double createInterpolatedPlaneCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr, Eigen::Vector3f plane_normal, double plane_dist, double x_max, double y_max, double x_min, double y_min) {
+    //generate points in the corresponding plane:
+    //double plane_z_interp(double x, double y, double plane_dist, Eigen::Vector3d normal)
+
+    input_cloud_ptr->points.clear();
+    double zPlaneSum = 0;
+    int count = 0;
+    for (float x = x_min; x < x_max; x += 0.01) {
+        for (float y = y_min; y < y_max; y += 0.01) {
+            float z = plane_z_interp(x, y, plane_dist, plane_normal);
+            zPlaneSum += z;
+            count++;
+            input_cloud_ptr->points.push_back(pcl::PointXYZ(x, y, z));
+        }
+    }
+    //pcl::toROSMsg(*g_interpolated_plane_fit_cloud, g_interpolated_plane_fit_cloud_ros);
+    //g_interpolated_plane_fit_cloud_ros.header.frame_id = "RBIP_frame";
+    int n_fitted = input_cloud_ptr->points.size();
+    input_cloud_ptr->width = n_fitted;
+    input_cloud_ptr->height = 1;
+
+    return zPlaneSum / count;
+}
+
+void write_table_transform_launchfile(Eigen::Affine3f affine_plane_wrt_cam,string fname) {
+    ofstream outfile(fname.c_str());
+    /*
+     <launch>
+       <node pkg="tf2_ros" type="static_transform_publisher" name="system_ref_frame" args="0 0 0 0 0 0 1 rear_wheel_frame system_ref_frame" />
+     </launch>
+     <node pkg="tf2_ros" type="static_transform_publisher" name="system_ref_frame" args="0 0 0 0 0 0 1 rear_wheel_frame system_ref_frame" />
+     */
+    string offset,quat_string;
+    Eigen::Quaternionf orientation(affine_plane_wrt_cam.linear());
+    quat_string=to_string(orientation.x())+" "+to_string(orientation.y())+" "+to_string(orientation.z())+" "+to_string(orientation.w());
+    Eigen::Vector3f offset_vec = affine_plane_wrt_cam.translation();
+    offset=to_string(offset_vec[0])+" "+to_string(offset_vec[1])+" "+to_string(offset_vec[2]);
+    outfile<<"<launch>"<<endl;
+    outfile<<"<node pkg=\"tf2_ros\" type=\"static_transform_publisher\" name=\"table_frame\" args=\""<<offset<<" "<<quat_string<<" camera_depth_optical_frame table_frame\" /> "<<endl;
+    outfile<<"</launch>"<<endl;
+    ROS_INFO_STREAM("calibrated launch file written to "<<fname<<endl);
+}
+
 
 
 //Callback functions
 geometry_msgs::Pose centroidCB(alpha_final::FindCentroidServiceRequest& request, alpha_final::FindCentroidServiceResponse& response){
-	
+
 } 
 
 //Main function 
@@ -63,7 +242,6 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "centroid_service");
 	ros::NodeHandle n;
 
-	ros::ServiceClient pubdesClient = n.serviceClient<mobot_pub_des_state::path>("append_path_queue_service");
 	ros::ServiceServer service = n.advertiseService("centroid_service",centroidCB);
 
 	ros::spin();
